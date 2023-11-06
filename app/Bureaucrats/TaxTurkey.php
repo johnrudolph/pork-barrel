f2aad82cd13c601250d5684388197c8796740891
johnrudolph <johnrudolphdrexler@gmail.com>
2023-11-05 21:40:03 -0500

    WIP

---
+++ app/Bureaucrats/TaxTurkey.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Bureaucrats;
+
+class TaxTurkey extends Bureaucrat
+{
+    const NAME = "Treasury Chicken";
+
+    const SHORT_DESCRIPTION = "Buy a treasury bond, and earn interest later.";
+
+    const DIALOG = "A penny saved is a penny earned.";
+
+    const EFFECT = "The winning bidder will spend the money now, and at the end of the game will receive their money back with 50% interest (rounded down).";
+}
